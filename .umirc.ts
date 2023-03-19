6efbfac44fe7bffbd624e3c443f37b6b97ab0e42
trumandu <1753177225@qq.com>
2023-03-19 19:54:53 +0800

    sync

--- .umirc.ts
+++ .umirc.ts
@@ -6,8 +6,16 @@ export default defineConfig({
     { path: "/about", component: "about" },
     { path: "/project", component: "project" },
   ],
-
+  hash: true,
   npmClient: "pnpm",
   tailwindcss: {},
   plugins: ["@umijs/plugins/dist/tailwindcss"],
+  headScripts: [
+    `(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?650d6d332fde7d8d1c2da71da2c598b5";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();`,
+  ],
 });
